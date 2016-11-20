8714244036bac597fb4171e5fa4fe7343ce35273
Corbin <corbin.coleman@holbertonschool.com>
2016-11-20 20:58:59 +0000

    Start of necessary function files to recreate printf function

---
+++ print_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_number - Print the given int one digit at a time
+ * @n: The integer to print
+ */
+int print_number(int n)
+{
+	int chars;
+
+	chars = 0;
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	else if (n > 0)
+		n *= -1;
+	else
+		_putchar('-');
+	if ((n / 10) != 0)
+	{
+		chars = print_number((n / 10) * -1);
+	}
+	_putchar((n % 10) * -1 + '0');
+	chars += 1;
+	return (chars);
+}
